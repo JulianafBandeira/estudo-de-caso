d2254e1020e31c5d15bd60ad8e46cbdb331ea3bd
JulianafBandeira <julianafbbarbosa@gmail.com>
2024-02-03 15:10:18 -0300

    conexao banco

---
+++ src/main/java/com/cursouml/estudodecaso/EstudodecasoApplication.java
@@ -0,0 +1,13 @@
+package com.cursouml.estudodecaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudodecasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudodecasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursouml/estudodecaso/EstudodecasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursouml.estudodecaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudodecasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
