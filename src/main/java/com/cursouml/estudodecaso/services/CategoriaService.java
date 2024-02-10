d7bf4a4e5a9be7d8ecf8575ae0f47d416c9cd99d
JulianafBandeira <julianafbbarbosa@gmail.com>
2024-02-10 10:36:18 -0300

    ajustes no endpoint/categorias/{id}

--- src/main/java/com/cursouml/estudodecaso/services/CategoriaService.java
+++ src/main/java/com/cursouml/estudodecaso/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cursouml.estudodecaso.domain.Categoria;
 import com.cursouml.estudodecaso.repositories.CategoriaRepository;
+import com.cursouml.estudodecaso.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName()));
 	}
 }
