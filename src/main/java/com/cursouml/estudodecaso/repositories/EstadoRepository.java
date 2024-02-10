c2bdd3783c5e82042f77da46b31925ecce8eb069
JulianafBandeira <julianafbbarbosa@gmail.com>
2024-02-10 13:58:45 -0300

    estado e cidade

---
+++ src/main/java/com/cursouml/estudodecaso/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.cursouml.estudodecaso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursouml.estudodecaso.domain.Categoria;
+import com.cursouml.estudodecaso.domain.Estado;
+
+@Repository
+public interface EstadoRepository  extends JpaRepository<Estado, Integer>{
+	
+
+}
