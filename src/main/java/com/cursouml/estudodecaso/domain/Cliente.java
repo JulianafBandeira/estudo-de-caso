e84def82b56d21d7c9dbaa7f38e05487f82cc564
JulianafBandeira <julianafbbarbosa@gmail.com>
2024-02-17 16:03:35 -0300

    pedido, estadopagamento e pagamento

--- src/main/java/com/cursouml/estudodecaso/domain/Cliente.java
+++ src/main/java/com/cursouml/estudodecaso/domain/Cliente.java
@@ -40,6 +40,8 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 
 	}
@@ -108,6 +110,14 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -125,6 +135,6 @@ public class Cliente implements Serializable{
 		Cliente other = (Cliente) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 
+	
 }
