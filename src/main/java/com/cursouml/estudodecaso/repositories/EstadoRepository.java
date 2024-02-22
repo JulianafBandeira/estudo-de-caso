a7100e4540c59c2a2a7812100d69b84f99e6e0b6
JulianafBandeira <julianafbbarbosa@gmail.com>
2024-02-22 10:34:33 -0300

    mapeamento pedido

--- src/main/java/com/cursouml/estudodecaso/repositories/EstadoRepository.java
+++ src/main/java/com/cursouml/estudodecaso/repositories/EstadoRepository.java
@@ -3,7 +3,6 @@ package com.cursouml.estudodecaso.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cursouml.estudodecaso.domain.Categoria;
 import com.cursouml.estudodecaso.domain.Estado;
 
 @Repository
